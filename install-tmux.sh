9216b154202a527e1dec405771214d35fb360529
Joao Machado <jpfogato@gmail.com>
2025-11-22 10:52:02 +0100

    version prior to updating installation script using stow

---
+++ install-tmux.sh
@@ -0,0 +1,10 @@
+# Set the tpm directory
+TPM_DIR="${HOME}/.tmux/plugins/tpm"
+
+# Download and install tpm if it's not there yet
+if [ ! -d "TPM_DIR" ]; then
+    mkdir -p "$(dirname $TPM_DIR)"
+    git clone https://github.com/zdharma-continuum/zinit.git "$TPM_DIR"
+fi
+
+
