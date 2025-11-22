8deb16719dfab3f35c2869015c04cd496a8e37cc
Joao Machado <jpfogato@gmail.com>
2025-11-22 10:58:16 +0100

    fixed install script for tmux

--- install-tmux.sh
+++ install-tmux.sh
@@ -4,7 +4,7 @@ TPM_DIR="${HOME}/.tmux/plugins/tpm"
 # Download and install tpm if it's not there yet
 if [ ! -d "TPM_DIR" ]; then
     mkdir -p "$(dirname $TPM_DIR)"
-    git clone https://github.com/zdharma-continuum/zinit.git "$TPM_DIR"
+     git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
 
